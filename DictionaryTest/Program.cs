b7578ae97f481927daf271a2799303ce8baa11bc
IlyaBasarab <town_prok@mail.ru>
2022-03-06 22:13:47 +0100

    Update Program.cs

--- DictionaryTest/Program.cs
+++ DictionaryTest/Program.cs
@@ -12,26 +12,6 @@ namespace DictionaryTest
         {
             Menu menu = new Menu();
             menu.Start();
-
-
-            //Dictionary<string, List<string>> dictionary = new Dictionary<string, List<string>>();
-
-            //string word = "slovo";
-            //string translate = "word";
-            //dictionary.Add(word, new List<string>());
-
-            //dictionary[word].Add(translate);
-
-            //foreach (var value in dictionary)
-            //{
-            //    Console.WriteLine(value.Key + ": ");
-            //    foreach(var transl in value.Value)
-            //    {
-            //        Console.WriteLine(transl);
-            //    }
-            //}
-
-
         }
     }
 }
